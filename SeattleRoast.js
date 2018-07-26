73a06b94d2cc2ad8deef0f40c79af210b7351db3
amchevannes <amber.chevannes@girlswhocode.com>
2018-07-26 14:40:56 -0700

    Update SeattleRoast.js

--- SeattleRoast.js
+++ SeattleRoast.js
@@ -5,3 +5,17 @@ function enlarge(word){
 function smaller(word){
   word.style.fontSize= "10px";
 }
+
+function makeGrey(x){
+  x.style.backgroundColor= "#999999";
+}
+
+function makeBlack(x){
+  x.style.backgroundColor= "black";
+}function enlarge(word){
+  word.style.fontSize= "30px";
+}
+
+function smaller(word){
+  word.style.fontSize= "10px";
+}
